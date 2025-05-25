486ed7d9bf658540da16557f0f577108e7be142e
Taehee Kim <taehee.kim@sherpas.team>
2025-05-25 18:25:01 +0900

    chore: init Article domain

---
+++ src/domain/articles/Article.ts
@@ -0,0 +1,13 @@
+export type Article = {
+  id: string;
+  title: string;
+  slug: string;
+  description: string;
+  body: string;
+//   author: string;
+//   tagList: string;
+//   favorited: string;
+//   favoritesCount: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
